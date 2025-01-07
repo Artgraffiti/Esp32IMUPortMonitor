b4431abeaf9201734a1f53fb240748c8bfa3dd90
Alexandr Vasilev <sanchous.vas@gmail.com>
2025-01-07 20:56:07 +0300

    added buttons

--- source/myprog.cc
+++ source/myprog.cc
@@ -1,8 +1,10 @@
+#include <cstddef>
 #include <iostream>
 #include <cstring>
 
 #include "window.h"
 #include "text.h"
+#include "list.h"
 #include "image.h"
 #include "button.h"
 #include "GUI.h"
@@ -14,19 +16,19 @@
 
 #define WIN_WIDTH 1200
 #define WIN_HEIGHT 800
-#define OUT_FIELD_WIDTH 300
-#define OUT_FIELD_HEIGHT 50
-#define BTN_CALCULATE_WIDTH OUT_FIELD_WIDTH
-#define BTN_CALCULATE_HEIGHT 50
+#define WIN_BCK_COLOR H_RGB(0x0D1117)
+#define BTNS_COLOR H_RGB(0x010409)
+#define BTN_PORT_MONITOR_WIDTH WIN_WIDTH / 2
+#define BTN_PORT_MONITOR_HEIGHT 60
 
 enum UserEventType {
-    EVENT_0,
-    EVENT_1,
+    EVENT_BTN_PORT_MONITOR,
+    EVENT_BTN_VISUALIZATION,
 };
 
 class MainWindow : public Window {
    public:
-    MainWindow() {}
+    MainWindow() { m_ClassName = __FUNCTION__; }
     ~MainWindow() {}
 
     void OnCreate();
@@ -35,20 +37,32 @@ class MainWindow : public Window {
     void OnNotify(Window *child, uint32_t type, const Point &position);
     void OnSizeChanged();
 
-    void ProcessDigit(uint16_t digit);
-    void PrintString();
-
    private:
-    char o_field[80];
+    TextButton *m_pBtnPortMonitor, *m_pBtnVisualization;
 };
 
 void MainWindow::OnCreate() {
     std::cout << "MainWindow::OnCreate()" << std::endl;
 
-    AddChild(new TextButton("Расчитать", EVENT_0),
-             Point(WIN_WIDTH / 2 - (BTN_CALCULATE_WIDTH / 2),
-                   WIN_HEIGHT / 2 - (BTN_CALCULATE_HEIGHT / 2)),
-             Rect(BTN_CALCULATE_WIDTH, BTN_CALCULATE_HEIGHT));
+    TextButton *pBtn;
+    
+    pBtn = new TextButton("Монитор порта", EVENT_BTN_PORT_MONITOR);
+    AddChild(pBtn, Point(0, 0), Rect(BTN_PORT_MONITOR_WIDTH, BTN_PORT_MONITOR_HEIGHT));
+    pBtn->SetBackColor(BTNS_COLOR);
+    pBtn->SetDarkColor(BTNS_COLOR);
+    pBtn->SetLiteColor(BTNS_COLOR);
+    pBtn->SetFrameColor(H_RGB(0x31363D));
+    pBtn->SetFontSize(26);
+    pBtn->SetTextColor(H_RGB(0xffffff));
+
+    pBtn = new TextButton("Визуализация", EVENT_BTN_VISUALIZATION);
+    AddChild(pBtn, Point(BTN_PORT_MONITOR_WIDTH, 0), Rect(BTN_PORT_MONITOR_WIDTH, BTN_PORT_MONITOR_HEIGHT));
+    pBtn->SetBackColor(BTNS_COLOR);
+    pBtn->SetDarkColor(BTNS_COLOR);
+    pBtn->SetLiteColor(BTNS_COLOR);
+    pBtn->SetFrameColor(H_RGB(0x31363D));
+    pBtn->SetFontSize(26);
+    pBtn->SetTextColor(H_RGB(0xffffff));
 
     CaptureKeyboard(this);
 }
@@ -57,7 +71,7 @@ void MainWindow::OnDraw(Context *cr) {
     std::cout << "MainWindow::OnDraw()" << std::endl;
     Point wsize = GetInteriorSize();
 
-    cr->SetColor(H_RGB(0x0e1a4f));
+    cr->SetColor(WIN_BCK_COLOR);
     cr->FillRectangle(Point(0, 0), wsize);
 }
 
@@ -65,8 +79,8 @@ void MainWindow::OnNotify(Window *child, uint32_t type, const Point &position) {
     std::cout << "MainWindow::OnNotify()" << std::endl;
 
     switch (type) {
-        case EVENT_0:
-
+        case EVENT_BTN_PORT_MONITOR:
+        case EVENT_BTN_VISUALIZATION:
         default:;
     }
 }
@@ -82,8 +96,6 @@ bool MainWindow::OnKeyPress(uint64_t keyval) {
 }
 
 void MainWindow::OnSizeChanged() {}
-
-void ProcessDigit(uint16_t digit) {}
 
 int main(int argc, char **argv) {
     MainWindow *pWindow = new MainWindow;
